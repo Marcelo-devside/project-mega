8ed43eb005ad41e83561a1b9f908a9264fdf5b07
marcelo <maxcelu@hotmail.com>
2022-07-16 16:28:44 -0300

    keyboard remove2

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const valuePar = document.querySelector("#value-par");
 const qtdImpares = document.querySelector("#qtdimpares");
 const valueImpares = document.querySelector("#value-impares");
 const legend = document.querySelector(".legend");
+const keyboardRemove = document.querySelector("#keyboardRemove");
 
 const removeDisabled = () => butao.removeAttribute("disabled");
 const addDisplayNone = () => (legend.style.display = "none");
@@ -41,7 +42,7 @@ input.addEventListener("input", (e) => {
   e.currentTarget.value = value;
   if (value.length == 17) {
     removeDisabled();
-    butao.focus();
+    keyboardRemove.setAttribute("checked", "checked")
   } else {
     addDisabled();
     addDisabled2();
